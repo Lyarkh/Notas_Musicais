52aac08526579fc83878ff975a5f6aa6ca62bc98
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-04-10 21:26:22 -0300

    fix: corrigindo bug referente a nota não existente

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -11,6 +11,10 @@ def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
         tonalidade: Tonalidade da escala
     Returns:
         Um dicionário com as notas da escala e os graus.
+
+    Raises:
+        ValueError: Caso a tônica não seja uma nota válida
+
     Examples:
         >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
@@ -21,7 +25,11 @@ def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """
     tonica = tonica.upper()
     intervalos = ESCALAS[tonalidade]
-    tonica_pos = NOTAS.index(tonica)
+
+    try:
+        tonica_pos = NOTAS.index(tonica)
+    except ValueError:
+        raise ValueError(f'Essa nota não existe, tente uma dessas {NOTAS}')
 
     temp = []
     for intervalo in intervalos:
