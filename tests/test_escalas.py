90316f1b72073d987adf24b064c37143eab83c6d
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-04-11 21:51:40 -0300

    test: teste referente caso a tonalidade não existe

--- tests/test_escalas.py
+++ tests/test_escalas.py
@@ -6,7 +6,7 @@ Arrumar - Agir - Garantir!
 """
 from pytest import raises
 
-from notas_musicais.escalas import NOTAS, escala
+from notas_musicais.escalas import ESCALAS, NOTAS, escala
 
 
 def test_deve_funcionar_com_notas_minusculas():
@@ -28,6 +28,18 @@ def test_deve_retornar_uma_error_dizendo_que_a_nota_nao_existe():
     mensagem_de_error = f'Essa nota não existe, tente uma dessas {NOTAS}'
 
     with raises(ValueError) as error:
+        escala(tonica, tonalidade)
+
+    assert mensagem_de_error == error.value.args[0]
+
+
+def test_deve_retornar_uma_error_dizendo_que_a_escala_nao_existe():
+    tonica = 'c'
+    tonalidade = 'tonalidade'
+
+    mensagem_de_error = f'Essa escala não existe ou não foi implementada. Tente uma dessas {list(ESCALAS.keys())}'
+
+    with raises(KeyError) as error:
         escala(tonica, tonalidade)
 
     assert mensagem_de_error == error.value.args[0]
