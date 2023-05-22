58d22a4519a329bd7e68880ecdb1b27840782d48
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-22 11:28:42 -0300

    feat: adicionando funcionalidade de triades e semitom
    criação de função que traz a nota um semiton a menos para correção de notas do diminuto

--- notas_musicais/acordes.py
+++ notas_musicais/acordes.py
@@ -1,4 +1,15 @@
-from notas_musicais.escalas import escala
+from notas_musicais.escalas import NOTAS, escala
+
+def semitom(nota, intervalo):
+    pos = NOTAS.index(nota) + intervalo
+
+    return NOTAS[pos % 12]
+
+def triade(nota, tonalidade):
+    graus = (0, 2, 4)
+    notas_da_escala, _ = escala(nota, tonalidade).values()
+
+    return [notas_da_escala[grau] for grau in graus]
 
 
 def acorde(cifra):
@@ -7,9 +18,17 @@ def acorde(cifra):
         >>> acorde('C')
         {'notas': ['C', 'E', 'G'], 'graus': ['I', 'III', 'V']}
     """
-    graus = (0, 2, 4)
-    notas_da_escala, _ = escala(cifra, 'maior').values()
+    if 'm' in cifra:
+        nota, _ = cifra.split('m')
+        notas = triade(nota, 'menor')
+        graus = ['I', 'III-', 'V']
+    elif 'º' in cifra:
+        nota, _ = cifra.split('º')
+        tonica, terca, quinta = triade(nota, 'menor')
+        notas = [tonica, terca, semitom(quinta, -1)]
+        graus = ['I', 'III-', 'V-']
+    else:
+        notas = triade(cifra, 'maior')
+        graus = ['I', 'III', 'V']
 
-    notas = [notas_da_escala[grau] for grau in graus]
-
-    return {'notas': notas, 'graus': ['I', 'III', 'V']}
+    return {'notas': notas, 'graus': graus}
