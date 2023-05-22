923f82933ff5c268c0926abd4aab82e62eff0871
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-22 11:26:15 -0300

    feat: adicionando escalas 'menor'

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -1,5 +1,5 @@
 NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
-ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
+ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11), 'menor': (0, 2, 3, 5, 7, 8, 10)}
 
 
 def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
